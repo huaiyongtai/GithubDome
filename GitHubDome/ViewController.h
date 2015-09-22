f02aed1068d72ddecb3d9ee203ca2f8265900bfb
HuaiYongtai <memorywarning@gmail.com>
2015-09-22 16:21:12 +0800

    初始化项目

---
+++ GitHubDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubDome
+//
+//  Created by HelloWorld on 15/9/22.
+//  Copyright (c) 2015年 无法修盖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubDome/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHubDome
+//
+//  Created by HelloWorld on 15/9/22.
+//  Copyright (c) 2015年 无法修盖. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
